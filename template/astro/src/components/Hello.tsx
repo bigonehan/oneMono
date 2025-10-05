eaa04ee46a0e514fe9d4d16ec5a59067b9ffb387
bigonehan <bigonehan@gmail.com>
2025-10-06 00:36:11 +0900

    feat: astro template

---
+++ template/astro/src/components/Hello.tsx
@@ -0,0 +1,8 @@
+import { HelloUI } from '@ui/shadcn/menu/HelloUI.tsx'
+export function Hello() {
+    return (
+        <div> This is Hello Component import Shadcn UI.
+            <HelloUI />
+        </div>
+    )
+}
