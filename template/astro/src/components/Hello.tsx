cc5a26c14ab1cab37849c70b1350c3574b48978f
bigonehan <bigonehan@gmail.com>
2025-10-07 15:55:49 +0900

    fix: load ui comonent applying tailwind

--- template/astro/src/components/Hello.tsx
+++
@@ -1,8 +0,0 @@
-import { HelloUI } from '@ui/shadcn/menu/HelloUI.tsx'
-export function Hello() {
-    return (
-        <div> This is Hello Component import Shadcn UI.
-            <HelloUI />
-        </div>
-    )
-}
