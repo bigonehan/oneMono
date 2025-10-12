593e3f73ed4d7e0f5b31a6c674b70464ed835bb3
bigonehan <bigonehan@gmail.com>
2025-10-13 02:48:37 +0900

    fix: unresolve component dependencies

--- packages/ui/shadcn/components/hero/hero_1.tsx
+++ packages/ui/shadcn/components/hero/hero_1.tsx
@@ -1,22 +1,21 @@
-import Link from "next/link"
-import Image from "next/image"
+
 import { cn } from "../../lib/utils"
 import { buttonVariants } from "@ui/shadcn/ui/button"
-import { heroHeader } from "@/config/contents"
+
 
 export type HeroConfig = {
-    header: string; // 메인 헤더 텍스트
-    subheader: string; // 서브 헤더 텍스트
-    image: string; // 이미지 파일 경로 (예: `/hero-img.webp`)
-    ctaLink: string; // "Get started" 버튼의 링크
-    ctaText: string; // "Get started" 버튼의 텍스트
+    header: string;
+    subheader: string;
+    ctaText: string;
 };
+
 interface HeroHeaderProps {
     config: HeroConfig;
+    ctaButton: React.ReactNode; // CTA 버튼을 슬롯으로
+    heroImage?: React.ReactNode; // 이미지를 슬롯으로 (optional)
 }
 
-// config를 props로 받도록 수정
-export default function HeroHeader({ config }: HeroHeaderProps) {
+export default function HeroHeader({ config, ctaButton, heroImage }: HeroHeaderProps) {
     return (
         <section className="container flex flex-col gap-4 pb-12 pt-4 text-center lg:items-center lg:gap-8 lg:py-20">
             <div className="flex flex-1 flex-col items-center gap-4 text-center lg:gap-8">
@@ -28,28 +27,13 @@ export default function HeroHeader({ config }: HeroHeaderProps) {
                         {config.subheader}
                     </h2>
                 </div>
-                <Link
-                    href={config.ctaLink}
-                    target="_blank"
-                    className={`w-[10rem] ${cn(buttonVariants({ size: "lg" }))}`}
-                >
-                    {config.ctaText}
-                </Link>
+                {ctaButton}
             </div>
-            {config.image !== "" ? (
+            {heroImage && (
                 <div className="flex flex-1 justify-center lg:justify-end">
-                    <Image
-                        src={config.image}
-                        width={500}
-                        height={500}
-                        alt="Header image"
-                    />
+                    {heroImage}
                 </div>
-            ) : (
-                <></>
             )}
         </section>
     )
 }
-
-
