b83689dc53aa7da9efda5c8eaae54bc02c76706b
bigonehan <bigonehan@gmail.com>
2025-10-13 00:08:01 +0900

    fix: page unresolve context files

--- template/next/app/page.tsx
+++ template/next/app/page.tsx
@@ -1,16 +1,6 @@
 import Image from "next/image";	
 import HeroHeader from "@ui/shadcn/hero/hero_1";
-import { HeroConfig } from "@ui/shadcn/types/hero"
-
-export const heroHeaderConfig: HeroConfig = {
-  header: `Landing pages made easy`,
-  subheader: `Easy to setup. Customizable. Quick. Responsive.`,
-  image: `/hero-img.webp`,
-  ctaLink: "https://github.com/redpangilinan/next-shadcn-landing", 
-  ctaText: "Get started",
-}
-
-
+import { heroHeaderConfig } from "@/config/heroHeaderConfig";
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
---
+++ template/next/src/config/heroHeaderConfig.ts
@@ -0,0 +1,9 @@
+import { HeroConfig } from "@ui/shadcn/types/hero";
+
+export const heroHeaderConfig: HeroConfig = {
+  header: `Landing pages made easy`,
+  subheader: `Easy to setup. Customizable. Quick. Responsive.`,
+  image: `/hero-img.webp`,
+  ctaLink: "https://github.com/redpangilinan/next-shadcn-landing", 
+  ctaText: "Get started",
+};
