ea1f5f59296ab606d2da88d1124994b6ec16cf3e
bigonehan <bigonehan@gmail.com>
2025-10-14 08:56:14 +0900

    feat: reset package

--- packages/configs/eslint/base.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
-  plugins: ["@typescript-eslint"],
-  parser: "@typescript-eslint/parser",
-  rules: {
-    "@typescript-eslint/no-unused-vars": "warn",
-  },
-};
