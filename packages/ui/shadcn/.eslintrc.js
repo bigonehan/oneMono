598ed912ea9f4b092cda3dde85c57e39834e2fd5
bigonehan <bigonehan@gmail.com>
2025-10-17 00:52:12 +0900

    feat(eslint): configure basic eslint for shadcn package

--- packages/ui/shadcn/.eslintrc.js
+++ packages/ui/shadcn/.eslintrc.js
@@ -1,3 +1,30 @@
 module.exports = {
-  extends: ["@configs/eslint/react.js"],
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks'],
+  rules: {
+    '@typescript-eslint/no-empty-function': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
