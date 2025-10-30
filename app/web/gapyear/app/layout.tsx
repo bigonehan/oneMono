5ce419ebdf4721a8575c40dd3f77d9dd6b0b00b3
bigonehan <bigonehan@gmail.com>
2025-10-30 16:46:43 +0900

    fix: .next config not found

--- app/web/gapyear/app/layout.tsx
+++ app/web/gapyear/app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Footer_1 } from "@ui/shadcn/footer/Footer_1";
 import "./globals.css";
 import { Providers } from "./providers";
 import { cn } from "@ui/shadcn/utils";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Ieum",
@@ -29,8 +18,6 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          geistSans.variable,
-          geistMono.variable,
           "min-h-screen bg-background text-foreground antialiased transition-colors duration-300"
         )}
       >
