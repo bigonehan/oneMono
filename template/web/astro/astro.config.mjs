73ceae4d90e319915cdbaac9f7407285e6c30be8
bigonehan <bigonehan@gmail.com>
2025-11-01 20:58:36 +0900

    feat: add components

--- template/web/astro/astro.config.mjs
+++ template/web/astro/astro.config.mjs
@@ -1,15 +1,29 @@
 // @ts-check
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path'
 
-import react from '@astrojs/react';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()],
-	},
-
   integrations: [react()],
-});
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        // tsconfig의 alias를 Vite에도 동일하게 알려줘야 함
+        '@': path.resolve('./src'),
+        '@ui/shadcn/components': path.resolve('../../../packages/ui/shadcn/components'),
+        '@ui/shadcn/globals.css': path.resolve('../../../packages/ui/shadcn/styles/globals.css'),
+        '@ui': path.resolve('../../../packages/ui'),
+        '@ui/shadcn': path.resolve('../../../packages/ui/shadcn'),
+      },
+    },
+    server: {
+      fs: {
+        // 모노레포 상위 폴더 접근 허용
+        allow: ['..'],
+      },
+    },
+  },
+})
