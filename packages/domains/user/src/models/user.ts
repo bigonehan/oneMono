a6bf69cbf506b2304ee415dafaac1dd4b2f4e135
bigonehan <bigonehan@gmail.com>
2026-05-16 20:58:49 +0900

    chore: clean up unused files

--- packages/domains/user/src/models/user.ts
+++ packages/domains/user/src/models/user.ts
@@ -1,12 +1,18 @@
-export type User = {
-  id: string;
-  name: string;
-  pw: string;
-  created_at: string;
-  modified_at: string;
-};
+import * as Schema from "effect/Schema";
 
-export type NewUser = {
-  name: string;
-  pw: string;
-};
+export const User = Schema.Struct({
+  id: Schema.String,
+  name: Schema.String,
+  pw: Schema.String,
+  created_at: Schema.String,
+  modified_at: Schema.String,
+});
+
+export type User = Schema.Schema.Type<typeof User>;
+
+export const NewUser = Schema.Struct({
+  name: Schema.String,
+  pw: Schema.String,
+});
+
+export type NewUser = Schema.Schema.Type<typeof NewUser>;
